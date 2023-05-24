681f63e4b338a1f80732d3c093012d3767eb6c9c
Aniket Sen <aniket.singh21@vsit.edu.in>
2023-05-24 14:20:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express')
-app.all("/",(req,res)=>{res.send("it works")})
-
-
+const app = express()
+app.all('/', (req, res) => {
+    console.log("Just got a request!")
+    res.send('Yo!')
+})
 app.listen(process.env.PORT || 3000)

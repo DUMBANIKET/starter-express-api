51957fb433de27051819cebeb0a8256113ef9709
Aniket Sen <aniket.singh21@vsit.edu.in>
2023-05-24 14:17:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,29 @@
 const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
+const express=require("express")
+const ytdl = require("ytdl-core")
+
+
+const app=express()
+app.use(express.json())
+
+app.all("/",(req,res)=>{res.send("i am working")})
+
+
+app.post("/video",async(req,res)=>{
+const url=await req.body.url
+const videoid=await ytdl.getURLVideoID(url)
+const meta=await ytdl.getBasicInfo(url)
+const store=meta.formats
+const quality=await store.map(r=>{
+    return(r.qualityLabel)
 })
+
+const videourl=await store.map(r=>{
+    return(`${r.url} and ${r.qualityLabel} `)
+})
+console.log(videourl)
+res.status(200).json(store)
+})
+
+
 app.listen(process.env.PORT || 3000)
